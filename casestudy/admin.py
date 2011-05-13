bab927c34ab028b11f12efdfbe7f2473beceed15
Peng gaojian <peng.gaojian@gmail.com>
2011-05-13 15:48:07 +0800

    delete all

--- casestudy/admin.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'gaojian'
-
-from django.contrib import admin
-from casestudy.models import Publisher, Author, Book
-
-admin.site.register(Publisher)
-admin.site.register(Author)
-admin.site.register(Book)
-  

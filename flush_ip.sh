1fe359d0b31e56795efe6c32659c903712a34a20
root <root@qiantai-linux.(none)>
2012-10-11 09:45:47 +0800

    ip changed

--- flush_ip.sh
+++ flush_ip.sh
@@ -5,7 +5,6 @@ cd "`dirname $0`"
 BASE=`pwd`
 ifconfig | grep  -A1 eth0 | tail -1 | awk '{print $2}' | awk -F: '{print $2}' > $BASE/ip.txt
 
-echo $BASE
 cd $BASE
 git commit -am "ip changed"
 git push origin
